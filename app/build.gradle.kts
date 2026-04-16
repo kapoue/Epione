620172443d4d4c0e9124f027b428a29bb217b1eb
kapoue <kapoue@gmail.com>
2026-04-16 12:16:02 +0200

    release: bump version 1.2.0 (lot-bonus-1 + lot-bonus-2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.epione.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
